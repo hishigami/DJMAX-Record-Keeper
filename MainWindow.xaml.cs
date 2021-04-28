af7b08037dbe16f8876b2b84a85bed1351857ecc
Hishigami <calvin.van@hotmail.com>
2021-04-27 21:08:25 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DJMAX_Record_Keeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ScoreRecord> scoreCollection = new ObservableCollection<ScoreRecord>();
+        ICollectionView ScoreList;
+        const string recordsFile = "records.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Attempt to deserialize json if it exists in base folder directory
+            if (File.Exists(recordsFile))
+            {
+                string json = File.ReadAllText(recordsFile);
+                var saved = JsonSerializer.Deserialize<ObservableCollection<ScoreRecord>>(json);
+                foreach (var record in saved)
+                {
+                    scoreCollection.Add(record);
+                }
+                txtMsg.Text = ("Successfully read saved records from disk.");
+            }
+
+            //Set defaults to song name and date
+            txtName.Text = "Ask to Wind";
+            dpDate.Value = new DateTime(2019, 12, 18);
+
+            //Set-up collection views
+            var scoreSourceList = new CollectionViewSource { Source = scoreCollection };
+            ScoreList = scoreSourceList.View;
+            //Create Predicate and add to ScoreList before binding it to DataGrid
+            var patternFilter = new Predicate<object>(pattern => ((ScoreRecord)pattern).PatternName.ToLower().Contains(txtSearch.Text.ToLower()));
+            ScoreList.Filter = patternFilter;
+            dgRecords.ItemsSource = ScoreList;
+        }
+
+        //Enforce non-null values for date by defaulting to EA start date
+        private void check_Date(object sender, RoutedEventArgs e)
+        {
+            if (dpDate.Value == null)
+            {
+                dpDate.Value = new DateTime(2019, 12, 18);
+            }
+        }
+
+        //Add score
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            //Null value check
+            if (txtName.Text == "" || dpDate.Value == null)
+            {
+                txtMsg.Text = ("A blank field was detected.\n" +
+                    "Please fill in the blank(s) with a value.");
+                return;
+            }
+
+            //Variable definitions
+            string song = txtName.Text;
+            //Here we probe for the radio option selected in both stack groups and ensure they have values
+            string selMode = stkMode.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value).Content.ToString();
+            string selDiff = stkDiff.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked.HasValue && r.IsChecked.Value).Content.ToString();
+            int score = (int)(intScore.Value);
+            double rate = (double)(dblRate.Value);
+            int breaks = (int)(intBreak.Value);
+            DateTime scoreDate = (DateTime)(dpDate.Value);
+
+            //Create ScoreRecord object and add to scoreCollection
+            ScoreRecord record = new ScoreRecord(song, selMode, selDiff, score, rate, breaks, scoreDate);
+            scoreCollection.Add(record);
+            txtMsg.Text = ("Record successfully added.");
+        }
+
+        //Reset all fields to default values
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            txtName.Text = "Ask to Wind";
+
+            rdo4.IsChecked = true;
+            rdoNm.IsChecked = true;
+
+            intScore.Value = 0;
+            dblRate.Value = 0.0;
+            intBreak.Value = 0;
+
+            dpDate.Value = new DateTime(2019, 12, 18);
+
+            txtMsg.Text = ("All fields have been reset to their defaults.");
+        }
+
+        //Refresh list for search queries
+        private void searchRecords(object sender, RoutedEventArgs e)
+        {
+            ScoreList.Refresh();
+        }
+
+        //Save all records to JSON
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            string json = JsonSerializer.Serialize(scoreCollection, options);
+            File.WriteAllText(recordsFile, json);
+            txtNotif.Text = "Records have been saved to disk.";
+        }
+
+        //Delete record after accepting warning
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult confirmDel = MessageBox.Show("Are you sure you want to delete this record?\n" +
+                "This cannot be undone!", "Confirm Record Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if(confirmDel == MessageBoxResult.Yes)
+            {
+                scoreCollection.Remove(dgRecords.SelectedItem as ScoreRecord);
+            }
+            txtNotif.Text = "Record has been deleted.";
+        }
+        
+    }
+}
---
+++ ScoreRecord.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJMAX_Record_Keeper
+{
+    //Currently, the program only covers the game's standard modes and difficulties
+    public class ScoreRecord
+    {
+        //Rank cutlines
+        private const double bMin = 80.0;
+        private const double aMin = 90.0;
+        private const double sMin = 97.0;
+
+        //Fields
+        private string _songName;
+        private string _mode;
+        private string _difficulty;
+        private string _patternName;
+        private int _score;
+        private double _rate;
+        private string _rank;
+        private int _break;
+        private DateTime _date;
+
+        //Properties
+        public string SongName
+        {
+            get { return _songName; }
+            private set { _songName = value; }
+        }
+        public string Mode
+        {
+            get { return _mode; }
+            private set { _mode = value; }
+        }
+        public string Difficulty
+        {
+            get { return _difficulty; }
+            private set { _difficulty = value; }
+        }
+        public string PatternName
+        {
+            get { return _patternName; }
+            private set { _patternName = value; }
+        }
+        public int Score
+        {
+            get { return _score; }
+            private set { _score = value; }
+        }
+        public double Rate
+        {
+            get { return _rate; }
+            private set { _rate = value; }
+        }
+        public string Rank
+        {
+            get { return _rank; }
+            private set { _rank = value; }
+        }
+        public int Breaks
+        {
+            get { return _break; }
+            private set { _break = value; }
+        }
+        public DateTime Date
+        {
+            get { return _date; }
+            private set { _date = value; }
+        }
+
+        //Constructor
+        public ScoreRecord(string songName, string mode, string difficulty, int score, double rate, int breaks, DateTime date)
+        {
+            SongName = songName;
+            Mode = mode;
+            Difficulty = difficulty;
+            PatternName = SongName + " " + Mode + Difficulty;
+            Score = score;
+            Rate = rate;
+            Rank = measureRank(rate, breaks);
+            Breaks = breaks;
+            Date = date;
+        }
+
+        //Determine rank from rate and breaks
+        private string measureRank(double rate, int breaks)
+        {
+            //PP check
+            if (rate == 100.0 && breaks == 0)
+                return "PP";
+
+            //Not a PP. Begin constructing output rank
+            string output;
+            
+            if (rate >= sMin)
+                output = "S";
+            else if (rate >= aMin)
+                output = "A";
+            else if (rate >= bMin)
+                output = "B";
+            else
+                output = "C";
+
+            //MC check
+            if (breaks == 0)
+                output += " MC";
+            
+            return output;
+        }
+    }
+}
